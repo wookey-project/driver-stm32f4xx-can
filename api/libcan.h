6b8786372d08c407a56af645f8fd3d827440157d
Philippe Thierry <phil@reseau-libre.net>
2019-10-21 14:43:55 +0200

    [update] add posthooks to initialization phase. Add IRQ handler basics and upper layer handler prototype

--- api/libcan.h
+++ api/libcan.h
@@ -5,6 +5,52 @@
 #include "libc/types.h"
 #include "libc/syscall.h"
 
+
+/**********************************************************************
+ * externally supplied implementations prototypes
+ *
+ * WARNING: these functions MUST be defined in the binary
+ * which include the libDFU. These functions implement
+ * the backend storage access, which may vary depending on
+ * the overall system implementation and which is not, as a
+ * consequence, a DFU specific implementation.
+ **********************************************************************/
+/*
+ * Why using symbol resolution instead of callbacks ?
+ *
+ * Symbol resolution is made at link time, instead of requiring
+ * function pointers that need to be registered in a writable
+ * area of the application memory.
+ *
+ * A lot of security vulnerabilities are based on function pointers
+ * corruption using overflows on stack contexts, making ROP or
+ * any other uncontrolled execution flows possible.
+ *
+ * Moreover, linking directly to the symbol avoid a resolution of
+ * the callback address and help the compiler at optimization time.
+ */
+
+typedef enum {
+    CAN_EVENT_RX_FIFO0_MSG_PENDING,
+    CAN_EVENT_RX_FIFO0_FULL,
+    CAN_EVENT_RX_FIFO1_MSG_PENDING,
+    CAN_EVENT_RX_FIFO1_FULL,
+    CAN_EVENT_TX_MBOX0_COMPLETE,
+    CAN_EVENT_TX_MBOX1_COMPLETE,
+    CAN_EVENT_TX_MBOX2_COMPLETE,
+    CAN_EVENT_TX_MBOX0_ABORT,
+    CAN_EVENT_TX_MBOX1_ABORT,
+    CAN_EVENT_TX_MBOX2_ABORT,
+    CAN_EVENT_SLEEP,
+    CAN_EVENT_WAKUP_FROM_RX_MSG,
+    CAN_EVENT_ERROR
+} can_event_t;
+
+mbed_error_t can_event(can_event_t event);
+
+
+/**********************************************************************/
+
 typedef enum {
    CAN_PORT_1 = 1,
    CAN_PORT_2 = 2,
@@ -42,27 +88,27 @@ typedef struct {
 } can_context_t;
 
 /* declare device */
-mbed_error_t can_declare(__out can_context_t *ctx);
+mbed_error_t can_declare(__inout can_context_t *ctx);
 
 /* init device */
-mbed_error_t can_initialize(can_context_t *ctx);
+mbed_error_t can_initialize(__inout can_context_t *ctx);
 
 /* release device */
-mbed_error_t can_release(can_context_t *ctx);
+mbed_error_t can_release(__inout can_context_t *ctx);
 
 /* set filters */
-mbed_error_t can_set_filters(void);
+mbed_error_t can_set_filters(__in can_context_t *ctx);
 
 /* start the CAN (required after initialization) */
-mbed_error_t can_start(can_context_t *ctx);
+mbed_error_t can_start(__inout can_context_t *ctx);
 
 /* Stop the CAN */
-mbed_error_t can_stop(can_context_t *ctx);
+mbed_error_t can_stop(__inout can_context_t *ctx);
 
 /* send data into one of the CAN Tx FIFO */
-mbed_error_t can_xmit(void);
+mbed_error_t can_xmit(const __in can_context_t *ctx);
 
 /* get back data from one of the CAN Rx FIFO */
-mbed_error_t can_receive(void);
+mbed_error_t can_receive(const __in can_context_t *ctx);
 
 #endif/*!LIBCAN_H_*/
--- can.c
+++ can.c
@@ -9,8 +9,76 @@
 #include "generated/can1.h"
 #include "generated/can2.h"
 
+
+/*******************************************************************
+ * local static functions & helpers
+ */
+
+/*
+ * This is the driver local IRQ Handler for all the CAN device interrupts.
+ * This handler get back required infos from the kernel posthooks and the
+ * CAN device and call the upper layer handler with the corresponding flag(s)
+ */
+static void can_IRQHandler(uint8_t irq,
+                           uint32_t status,
+                           uint32_t data)
+{
+    uint32_t msr, ier, esr; /* status and control */
+    uint32_t tsr = 0, rf0r = 0, rf1r = 0; /* tx/rx */
+
+
+    /* get back CAN state (depending on current IRQ) */
+    switch (irq) {
+        case CAN1_TX_IRQ:
+            ier = read_reg_value(_r_CANx_IER(1));
+            esr = read_reg_value(_r_CANx_ESR(1));
+            msr = status;
+            tsr = data;
+            break;
+        case CAN1_RX0_IRQ:
+            ier = read_reg_value(_r_CANx_IER(1));
+            esr = read_reg_value(_r_CANx_ESR(1));
+            msr = status;
+            rf0r = data;
+            break;
+        case CAN1_RX1_IRQ:
+            ier = read_reg_value(_r_CANx_IER(1));
+            esr = read_reg_value(_r_CANx_ESR(1));
+            msr = status;
+            rf1r = data;
+            break;
+        case CAN1_SCE_IRQ:
+            ier = read_reg_value(_r_CANx_IER(1));
+            msr = status;
+            esr = data;
+            break;
+        default:
+            goto err;
+            break;
+    }
+
+    /* now handling current interrupt */
+    /********** handling transmit case ***************/
+    /* Tx Mbox 0 */
+    /* Tx Mbox 1 */
+    /* Tx Mbox 2 */
+    /********** handling receive case ***************/
+    /* Tx FIFO0 overrun */
+    /* Tx FIFO0 full */
+    /* Tx FIFO0 msg pending */
+    /* Tx FIFO1 overrun */
+    /* Tx FIFO1 full */
+    /* Tx FIFO1 msg pending */
+    /********** handling status change **************/
+    /* Sleep */
+    /* Wakeup */
+    /* Errors */
+err:
+    return;
+}
+
 /* declare device */
-mbed_error_t can_declare(__out can_context_t *ctx)
+mbed_error_t can_declare(__inout can_context_t *ctx)
 {
     mbed_error_t errcode = MBED_ERROR_INVSTATE;
     e_syscall_ret sret;
@@ -45,12 +113,96 @@ mbed_error_t can_declare(__out can_context_t *ctx)
                ctx->can_dev.irq_num = 0;
            } else {
                ctx->can_dev.irq_num = 4;
-               //ctx->can_dev.irqs[0].handler = ;
+               ctx->can_dev.irqs[0].handler = can_IRQHandler;
                ctx->can_dev.irqs[0].irq = CAN1_TX_IRQ;
                ctx->can_dev.irqs[0].mode = IRQ_ISR_STANDARD;
-               /* TODO: define posthooks:
-                  ctx->can_dev.irqs[0].posthook.
-                  */
+
+               /* RX0 interrupt is the consequence of RF0R register bit set,
+                * see ST RM00090, chap 32.8 (CAN Interrupts) fig. 348 */
+               ctx->can_dev.irqs[0].posthook.status = 0x0004; /* MSR */
+               ctx->can_dev.irqs[0].posthook.data   = 0x0008; /* TSR */
+
+               ctx->can_dev.irqs[0].posthook.action[0].instr = IRQ_PH_READ;
+               ctx->can_dev.irqs[0].posthook.action[0].read.offset = 0x0004;
+
+               ctx->can_dev.irqs[0].posthook.action[1].instr = IRQ_PH_READ;
+               ctx->can_dev.irqs[0].posthook.action[1].read.offset = 0x0008;
+
+               ctx->can_dev.irqs[0].posthook.action[2].instr = IRQ_PH_WRITE;
+               ctx->can_dev.irqs[0].posthook.action[2].write.offset = 0x0008;
+               ctx->can_dev.irqs[0].posthook.action[2].write.value  = 0x1;
+               ctx->can_dev.irqs[0].posthook.action[2].write.mask   = 0x1 << 0; /* clear TSR: RQCP0 */
+               ctx->can_dev.irqs[0].posthook.action[3].instr = IRQ_PH_WRITE;
+               ctx->can_dev.irqs[0].posthook.action[3].write.offset = 0x0008;
+               ctx->can_dev.irqs[0].posthook.action[3].write.value  = 0x1;
+               ctx->can_dev.irqs[0].posthook.action[3].write.mask   = 0x1 << 8; /* clear TSR: RQCP1 */
+               ctx->can_dev.irqs[0].posthook.action[4].instr = IRQ_PH_WRITE;
+               ctx->can_dev.irqs[0].posthook.action[4].write.offset = 0x0008;
+               ctx->can_dev.irqs[0].posthook.action[4].write.value  = 0x1;
+               ctx->can_dev.irqs[0].posthook.action[4].write.mask   = 0x1 << 16; /* clear TSR: RQCP2 */
+
+
+               /* RX0 interrupt is the consequence of RF0R register bit set,
+                * see ST RM00090, chap 32.8 (CAN Interrupts) fig. 348 */
+               ctx->can_dev.irqs[1].handler = can_IRQHandler;
+               ctx->can_dev.irqs[1].irq = CAN1_RX0_IRQ;
+               ctx->can_dev.irqs[1].mode = IRQ_ISR_STANDARD;
+
+               ctx->can_dev.irqs[1].posthook.status = 0x0004; /* MSR */
+               ctx->can_dev.irqs[1].posthook.data   = 0x000C; /* RF0R */
+
+               ctx->can_dev.irqs[1].posthook.action[0].instr = IRQ_PH_READ;
+               ctx->can_dev.irqs[1].posthook.action[0].read.offset = 0x0004;
+
+               ctx->can_dev.irqs[1].posthook.action[1].instr = IRQ_PH_READ;
+               ctx->can_dev.irqs[1].posthook.action[1].read.offset = 0x000C;
+
+               ctx->can_dev.irqs[1].posthook.action[2].instr = IRQ_PH_WRITE;
+               ctx->can_dev.irqs[1].posthook.action[2].write.offset = 0x000C;
+               ctx->can_dev.irqs[1].posthook.action[2].write.value  = 0x03;
+               ctx->can_dev.irqs[1].posthook.action[2].write.mask   = 0x3 << 3; /* clear RF0R:FULL0, RF0R:FOVR0 */
+
+
+               /* RX1 interrupt is the consequence of RF1R register bit set,
+                * see ST RM00090, chap 32.8 (CAN Interrupts) fig. 348 */
+               ctx->can_dev.irqs[2].handler = can_IRQHandler;
+               ctx->can_dev.irqs[2].irq = CAN1_RX1_IRQ;
+               ctx->can_dev.irqs[2].mode = IRQ_ISR_STANDARD;
+
+               ctx->can_dev.irqs[2].posthook.status = 0x0004; /* MSR */
+               ctx->can_dev.irqs[2].posthook.data   = 0x0010; /* RF1R */
+
+               ctx->can_dev.irqs[2].posthook.action[0].instr = IRQ_PH_READ;
+               ctx->can_dev.irqs[2].posthook.action[0].read.offset = 0x0004;
+
+               ctx->can_dev.irqs[2].posthook.action[1].instr = IRQ_PH_READ;
+               ctx->can_dev.irqs[2].posthook.action[1].read.offset = 0x0010;
+
+               ctx->can_dev.irqs[2].posthook.action[2].instr = IRQ_PH_WRITE;
+               ctx->can_dev.irqs[2].posthook.action[2].write.offset = 0x0010;
+               ctx->can_dev.irqs[2].posthook.action[2].write.value  = 0x03;
+               ctx->can_dev.irqs[2].posthook.action[2].write.mask   = 0x3 << 3; /* clear RF1R:FULL1, RF1R:FOVR1 */
+
+               /* SCE interrupt is the consequence of MSR register bit set,
+                * in association with the ESR register filters
+                * see ST RM00090, chap 32.8 (CAN Interrupts) fig. 348 */
+               ctx->can_dev.irqs[3].handler = can_IRQHandler;
+               ctx->can_dev.irqs[3].irq = CAN1_SCE_IRQ; /* status change*/
+               ctx->can_dev.irqs[3].mode = IRQ_ISR_STANDARD;
+
+               ctx->can_dev.irqs[3].posthook.status = 0x0004; /* MSR */
+               ctx->can_dev.irqs[3].posthook.data   = 0x0018; /* ESR */
+
+               ctx->can_dev.irqs[3].posthook.action[0].instr = IRQ_PH_READ;
+               ctx->can_dev.irqs[3].posthook.action[0].read.offset = 0x0004;
+
+               ctx->can_dev.irqs[3].posthook.action[1].instr = IRQ_PH_READ;
+               ctx->can_dev.irqs[3].posthook.action[1].read.offset = 0x0018;
+
+               ctx->can_dev.irqs[3].posthook.action[2].instr = IRQ_PH_WRITE;
+               ctx->can_dev.irqs[3].posthook.action[2].write.offset = 0x0004;
+               ctx->can_dev.irqs[3].posthook.action[2].write.value  = 0x00;
+               ctx->can_dev.irqs[3].posthook.action[2].write.mask   = 0x7 << 2; /* clear SLAKI, WKUI & ERRI (previous values saved in status variable */
            }
            break;
         case CAN_PORT_2:
@@ -85,7 +237,7 @@ end:
 }
 
 /* init device */
-mbed_error_t can_initialize(can_context_t *ctx)
+mbed_error_t can_initialize(__inout can_context_t *ctx)
 {
     volatile int check = 0;
     if (!ctx) {
@@ -172,7 +324,7 @@ mbed_error_t can_initialize(can_context_t *ctx)
 }
 
 /* release device */
-mbed_error_t can_release(can_context_t *ctx)
+mbed_error_t can_release(__inout can_context_t *ctx)
 {
     if (ctx == NULL) {
         return MBED_ERROR_INVPARAM;
@@ -191,13 +343,14 @@ mbed_error_t can_release(can_context_t *ctx)
 }
 
 /* set filters */
-mbed_error_t can_set_filters(void)
+mbed_error_t can_set_filters(__in can_context_t *ctx)
 {
+    ctx = ctx;
     return MBED_ERROR_NONE;
 }
 
 /* start the CAN (required after initialization) */
-mbed_error_t can_start(can_context_t *ctx)
+mbed_error_t can_start(__inout can_context_t *ctx)
 {
     int check = 0;
     if (ctx->state != CAN_STATE_READY) {
@@ -215,7 +368,7 @@ mbed_error_t can_start(can_context_t *ctx)
 }
 
 /* Stop the CAN */
-mbed_error_t can_stop(can_context_t *ctx)
+mbed_error_t can_stop(__inout can_context_t *ctx)
 {
     int check = 0;
     if (ctx == NULL) {
@@ -238,13 +391,18 @@ mbed_error_t can_stop(can_context_t *ctx)
 }
 
 /* send data into one of the CAN Tx FIFO */
-mbed_error_t can_xmit(void)
+mbed_error_t can_xmit(const __in can_context_t *ctx)
 {
+    ctx = ctx;
     return MBED_ERROR_NONE;
 }
 
 /* get back data from one of the CAN Rx FIFO */
-mbed_error_t can_receive(void)
+mbed_error_t can_receive(const __in can_context_t *ctx)
 {
+    ctx = ctx;
     return MBED_ERROR_NONE;
 }
+
+
+
